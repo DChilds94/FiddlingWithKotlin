2cb161a98c4fe50a3ebdddaf36131ec91df61197
Daniel Childs <dc.1994@outlook.com>
2019-06-12 11:43:01 +0100

    Understand how default Parameters and jvmOverload annoation works

--- src/App.java
+++ src/App.java
@@ -3,5 +3,8 @@ public class App {
 
     public static void main(String[] args) {
        DisplayFunctions.display("Hello, World from Java");
+       DisplayFunctions.log("Hello, World from Java", 3);
+       DisplayFunctions.log("Hello, World from Java");
     }
+
 }
--- src/kotlin/Program.kt
+++ src/kotlin/Program.kt
@@ -1,9 +1,15 @@
 @file:JvmName("DisplayFunctions")
 fun main(){
     display("Hello World, from Kotlin")
+    log("Hello World from log function")
 }
 
 fun display(message: String) : Boolean {
     println(message)
     return true
+}
+
+@JvmOverloads
+fun log(message: String, logLevel: Int = 1) {
+    println(message + logLevel)
 }
