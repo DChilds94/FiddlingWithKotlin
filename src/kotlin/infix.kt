b672b6391727f866b02eba37283e5533d19837ea
Daniel Childs <dc.1994@outlook.com>
2019-06-12 12:01:48 +0100

    fiddle with infix and operator overloading to understand how it's working

---
+++ src/kotlin/infix.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val h1 = Header("h1")
+    val h2 = Header("h2")
+    val h3 = h1 plus h2
+
+    println(h3.name)
+
+    val h4 = h1 + h2
+    println(h4.name)
+}
+
+class Header(var name: String) {}
+
+infix operator fun Header.plus(other: Header) : Header{
+    return Header(this.name + other.name)
+}
